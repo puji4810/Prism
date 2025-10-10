23b2d53f07febe53434452f1463797e15ea9cee6
puji4810 <2430438586@qq.com>
2025-10-10 17:07:34 +0800

    feat: Integrate Google Test framework and refactor unit tests for coding and database functionalities

--- xmake.lua
+++ xmake.lua
@@ -1,32 +1,43 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "."})
 
-add_requires("crc32c")
+add_requires("crc32c", "gtest")
 
 set_languages("cxx23")
 
-set_kind("binary")
-add_files("src/*.cpp")
-add_files("util/*.cpp") 
-add_includedirs("include", {public = true})
-add_includedirs("src", {public = true})
-add_includedirs("util", {public = true})
-add_packages("crc32c")
+local common_files = {"src/*.cpp", "util/*.cpp"}
+local common_includedirs = {"include", "src", "util"}
 
 target("prism")
+    set_kind("binary")
+    add_files(common_files)
+    add_files("tests/main.cpp")
+    add_includedirs(common_includedirs)
+    add_packages("crc32c")
+    
     if is_mode("debug") then
         set_symbols("debug")
         set_optimize("none")
         add_defines("DEBUG")
-        add_cflags("-g", "-O0")
     end
-    add_files("tests/main.cpp")
 
 target("main")
+    set_kind("binary")
+    add_files(common_files)
     add_files("tests/main.cpp")
-    
+    add_includedirs(common_includedirs)
+    add_packages("crc32c")
+
 target("coding_test")
+    set_kind("binary")
+    add_files(common_files)
     add_files("tests/coding_test.cpp")
+    add_includedirs(common_includedirs)
+    add_packages("crc32c", "gtest")
 
 target("db_test")
+    set_kind("binary")
+    add_files(common_files)
     add_files("tests/db_test.cpp")
+    add_includedirs(common_includedirs)
+    add_packages("crc32c", "gtest")
