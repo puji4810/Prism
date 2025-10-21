83a3b8e66c5d0b2b1c0a3ae303e879f48bffc247
puji4810 <2430438586@qq.com>
2025-10-22 02:10:55 +0800

    refactor: Move test targets to separate xmake.lua for better organization
    Reorganize test targets by moving them from the main xmake.lua to a dedicated tests/xmake.lua file. This improves project structure and maintainability. Update paths to ensure correct file inclusion.

--- tests/xmake.lua
+++ tests/xmake.lua
@@ -1,13 +1,43 @@
 -- Tests build targets for Prism
+-- All test targets are defined here and included from root xmake.lua
+
+local common_files = {"$(projectdir)/src/*.cpp", "$(projectdir)/util/*.cpp"}
+local common_includedirs = {"$(projectdir)/include", "$(projectdir)/src", "$(projectdir)/util"}
+
+target("coding_test")
+    set_kind("binary")
+    add_files(common_files)
+    add_files("$(projectdir)/tests/coding_test.cpp")
+    add_includedirs(common_includedirs)
+    add_packages("crc32c", "gtest")
+    add_tests("default")
+
+target("db_test")
+    set_kind("binary")
+    add_files(common_files)
+    add_files("$(projectdir)/tests/db_test.cpp")
+    add_includedirs(common_includedirs)
+    add_packages("crc32c", "gtest")
+    add_tests("default")
+
+target("skiplist_test")
+    set_kind("binary")
+    add_files("$(projectdir)/tests/skiplist_test.cpp", "$(projectdir)/util/arena.cpp")
+    add_packages("gtest")
+    add_includedirs("$(projectdir)/include", "$(projectdir)/util")
+    add_tests("default")
+
+target("arena_test")
+    set_kind("binary")
+    add_files("$(projectdir)/tests/arena_test.cpp", "$(projectdir)/util/arena.cpp")
+    add_packages("gtest")
+    add_includedirs("$(projectdir)/include", "$(projectdir)/util")
+    add_tests("default")
 
 target("dbformat_test")
     set_kind("binary")
-    -- paths relative to project root
     add_files("$(projectdir)/tests/dbformat_test.cpp")
-    -- explicitly add required implementation sources from project
     add_files("$(projectdir)/include/dbformat.cpp", "$(projectdir)/include/comparator.cpp", "$(projectdir)/util/coding.cpp")
     add_includedirs("$(projectdir)/include", "$(projectdir)/util")
     add_packages("gtest")
     add_tests("default")
-
-
