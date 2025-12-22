72a4d7058c301da8a9bec963c25cf4794154635b
puji4810 <2430438586@qq.com>
2025-12-22 15:27:46 +0800

    refactor(db): Update DB interface to use Result for error handling
    - Modify Get and Open methods in DB and DBImpl to return Result types instead of Status, improving error handling and clarity.
    - Update related methods and tests to accommodate the new Result type, ensuring consistent usage across the database interface.
    - Enhance unit tests to validate the new Result-based error handling in various scenarios, including successful and failed operations.
    - Refactor result handling in tests to align with the updated API, ensuring robust test coverage.

--- include/db.h
+++ include/db.h
@@ -1,10 +1,12 @@
 #ifndef PRISM_DB_H
 #define PRISM_DB_H
 
+#include <expected>
 #include <string>
 #include <memory>
 #include "options.h"
 #include "status.h"
+#include "result.h"
 #include "slice.h"
 #include "iterator.h"
 #include "write_batch.h"
@@ -16,18 +18,20 @@ namespace prism
 	public:
 		DB() = default;
 		virtual ~DB();
-		
+
 		// Open the database with the specified "name".
 		// Returns a pointer to a heap-allocated database on success.
-		// Returns nullptr on error.
-		static std::unique_ptr<DB> Open(const Options& opts, const std::string& dbname);
-		static std::unique_ptr<DB> Open(const std::string& dbname);
-		
+		// Returns a Result
+		static Result<std::unique_ptr<DB>> Open(const Options& opts, const std::string& dbname);
+		static Result<std::unique_ptr<DB>> Open(const std::string& dbname);
+
 		// Set the database entry for "key" to "value".
 		// Returns OK on success, and a non-OK status on error.
 		virtual Status Put(const WriteOptions& options, const Slice& key, const Slice& value) = 0;
+
+		// For all this kinds of api, we just wrap the Func(Options, Args...) by default options
 		Status Put(const Slice& key, const Slice& value) { return Put(WriteOptions(), key, value); }
-		
+
 		// If the database contains an entry for "key" store the
 		// corresponding value in *value and return OK.
 		//
@@ -35,15 +39,15 @@ namespace prism
 		// a status for which Status::IsNotFound() returns true.
 		//
 		// May return some other Status on an error.
-		virtual Status Get(const ReadOptions& options, const Slice& key, std::string* value) = 0;
-		Status Get(const Slice& key, std::string* value) { return Get(ReadOptions(), key, value); }
-		
+		virtual Result<std::string> Get(const ReadOptions& options, const Slice& key) = 0;
+		Result<std::string> Get(const Slice& key) { return Get(ReadOptions(), key); }
+
 		// Remove the database entry (if any) for "key".  Returns OK on
 		// success, and a non-OK status on error.  It is not an error if "key"
 		// did not exist in the database.
 		virtual Status Delete(const WriteOptions& options, const Slice& key) = 0;
 		Status Delete(const Slice& key) { return Delete(WriteOptions(), key); }
-		
+
 		// Apply the specified updates to the database.
 		// Returns OK on success, non-OK on failure.
 		virtual Status Write(const WriteOptions& options, WriteBatch* batch) = 0;
@@ -56,6 +60,8 @@ namespace prism
 		virtual const Snapshot* GetSnapshot() = 0;
 		virtual void ReleaseSnapshot(const Snapshot* snapshot) = 0;
 	};
+
+	Status DestroyDB(const std::string& dbname, const Options& options);
 } // namespace prism
 
 #endif // PRISM_DB_H
